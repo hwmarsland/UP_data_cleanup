50552adea9579f71c48a6494dd6894beadc7f3fd
hwmarsland <89931996+hwmarsland@users.noreply.github.com>
2022-05-18 14:52:16 -0400

    Add files via upload

---
+++ data_clean_up.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import sys
+
+def data_clean_up(filename):
+    raw_data = pd.read_excel(filename, index_col = 0)
+
+    edited_data = raw_data.copy()
+    edited_data = edited_data.replace('--', '—', regex=True)
+    edited_data = edited_data.replace('@-', '–', regex=True)
+    edited_data = edited_data.replace('###', '<i>', regex=True)
+    edited_data = edited_data.replace('# ', '</i> ', regex=True)
+    edited_data = edited_data.replace('‘', '\'', regex=True)
+    edited_data = edited_data.replace('’', '\'', regex=True)
+    edited_data = edited_data.replace('“', '"', regex=True)
+    edited_data = edited_data.replace('”', '"', regex=True)
+
+    writer = pd.ExcelWriter(str(filename)+' edited-data.xlsx', engine='xlsxwriter')
+    edited_data.to_excel(writer, sheet_name='Sheet1')
+    writer.save()
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Usage: python data_clean_up.py <filename>')
+    
+    else:
+        data_clean_up(sys.argv[1])
