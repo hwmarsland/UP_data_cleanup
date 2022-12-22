26fe2e753126eb5e72e88cfa80cd9e4ee4859a02
hwmarsland <89931996+hwmarsland@users.noreply.github.com>
2022-12-22 18:28:35 -0500

    Update data_clean_up_v1_5.py

--- data_clean_up_v1_5.py
+++ data_clean_up_v1_5.py
@@ -29,6 +29,8 @@ def data_clean_up(filename):
     writer = pd.ExcelWriter(str(filename)+' edited-data.xlsx', engine='xlsxwriter')
     edited_data.to_excel(writer, sheet_name='Sheet1')
     writer.save()
+    
+    print("Finished")
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
